735bad2216538e8d315cefd8bc7e0f688c7cd6dc
Diego_M <diegofernandomzlz@gmail.com>
2022-02-13 02:33:18 +0000

    domain-name-generator-v1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dotAlgo = [".com", ".io", ".net", ".us"];
+
+  //intentar hacer esta parte con una funcion cuando ya resulte todo bien
+  let pronounRandom = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let adjRandom = adj[Math.floor(Math.random() * adj.length)];
+  let nounRandom = noun[Math.floor(Math.random() * noun.length)];
+  let dotAlgoRandom = dotAlgo[Math.floor(Math.random() * dotAlgo.length)];
+  //
+  let domain = pronounRandom + adjRandom + nounRandom + dotAlgoRandom;
+  for (let domainN = 0; domainN < 10; domainN++) {
+    console.log(domain);
+  }
+  document.getElementById("domainList").innerHTML = domain;
 };
