0d039f302e9b97dfa78383858dba3c911167da14
Diego_M <diegofernandomzlz@gmail.com>
2022-02-14 20:49:18 +0000

    domain-generator-v2

--- src/app.js
+++ src/app.js
@@ -6,20 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let dotAlgo = [".com", ".io", ".net", ".us"];
+  let pronoun = ["the", "our", "one"];
+  let adj = ["great", "big", "super"];
+  let noun = ["jogger", "racoon", "idea"];
+  let dotAlgo = [".com", ".io", ".net", ".us", ".es"];
 
-  //intentar hacer esta parte con una funcion cuando ya resulte todo bien
-  let pronounRandom = pronoun[Math.floor(Math.random() * pronoun.length)];
-  let adjRandom = adj[Math.floor(Math.random() * adj.length)];
-  let nounRandom = noun[Math.floor(Math.random() * noun.length)];
-  let dotAlgoRandom = dotAlgo[Math.floor(Math.random() * dotAlgo.length)];
-  //
-  let domain = pronounRandom + adjRandom + nounRandom + dotAlgoRandom;
-  for (let domainN = 0; domainN < 10; domainN++) {
-    console.log(domain);
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dotAlgo.length; l++) {
+          var domainList = [pronoun[i] + adj[j] + noun[k] + dotAlgo[l]];
+          console.log(domainList);
+        }
+      }
+    }
   }
-  document.getElementById("domainList").innerHTML = domain;
+  document.getElementById("domainList").innerHTML = domainList;
 };
